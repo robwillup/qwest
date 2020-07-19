ffb0cbbbdad2c641900805829812fc80fcb6732c
Robson William <r.willup@hotmail.com>
2020-07-19 12:20:44 -0300

    Added initial logic to check command-line arguments

--- src/Quest/Program.cs
+++ src/Quest/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using System.Linq;
+using static System.Console;
 
 namespace Quest
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+                WriteLine("Welcome to the Quest!");
+            else if (args.Contains("new"))
+            {
+                if (!args.Contains("--todo"))
+                {
+                    WriteLine(@"When the 'new' command is used, the '--todo' flag must be provided, as well as its text.");
+                    return 1;
+                }
+                else
+                {
+                    int todoIndex = args.ToList().IndexOf("--todo");                    
+                    if (todoIndex + 1 < args.Length && !string.IsNullOrEmpty(args[todoIndex + 1]))
+                    {
+                        string todoText = args[todoIndex + 1];
+                        WriteLine(todoText);
+                    }
+                    else
+                        WriteLine("A text for the '--todo' flag must be provided.");
+                    
+                }
+            }
+            return 0;
         }
     }
 }
