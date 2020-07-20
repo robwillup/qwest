ed29bebffb9ee10a10f316a8d0e910633c30dcb8
Robson William <r.willup@hotmail.com>
2020-07-19 21:44:30 -0300

    Appending strings to a file in AppData\Roaming\Quest\todo.txt

--- src/Quest/Program.cs
+++ src/Quest/Program.cs
@@ -1,12 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.IO;
 using static System.Console;
 
 namespace Quest
 {
     class Program
     {
+        public static string TodoText { get; set; }
         static int Main(string[] args)
         {
+            // Refactor ASAP
             if (args.Length == 0)
                 WriteLine("Welcome to the Quest!");
             else if (args.Contains("new"))
@@ -18,14 +22,30 @@ namespace Quest
                 }
                 else
                 {
-                    int todoIndex = args.ToList().IndexOf("--todo");                    
+                    int todoIndex = args.ToList().IndexOf("--todo");
                     if (todoIndex + 1 < args.Length && !string.IsNullOrEmpty(args[todoIndex + 1]))
                     {
-                        string todoText = args[todoIndex + 1];
-                        WriteLine(todoText);
+                        TodoText = args[todoIndex + 1];                        
+                        string questTodosPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Quest";
+                        if (!Directory.Exists(questTodosPath)) 
+                        {
+                            Directory.CreateDirectory(questTodosPath);
+                            using (_ = File.Create(@$"{questTodosPath}\todos.txt")) {;}
+                        } 
+                        else if (!File.Exists(@$"{questTodosPath}\todos.txt"))
+                        {
+                            using (_ = File.Create(@$"{questTodosPath}\todos.txt")) {; }
+                        }
+                        using (StreamWriter sw = File.AppendText($@"{questTodosPath}\todos.txt"))                        
+                            sw.WriteLine(TodoText);
+                        
+                        return 0;
                     }
                     else
+                    {
                         WriteLine("A text for the '--todo' flag must be provided.");
+                        return 1;
+                    }
                     
                 }
             }
