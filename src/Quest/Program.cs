6019fd0d6f25aea13b14558bee4346ca519b8454
Robson William <r.willup@hotmail.com>
2020-07-21 20:50:21 -0300

    Refactored Main method a little bit more and Added a 'list' command

--- src/Quest/Program.cs
+++ src/Quest/Program.cs
@@ -8,46 +8,78 @@ namespace Quest
     class Program
     {
         public static string TodoText { get; set; }
+        public static string QuestTodosPath { get; set; }
+        
         static int Main(string[] args)
         {
-            // Refactor ASAP
+            QuestTodosPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Quest";
+            return CheckArgs(args);
+        }
+        public static int CheckArgs(string[] args)
+        {
             if (args.Length == 0)
                 WriteLine("Welcome to the Quest!");
-            else if (args.Contains("new"))
+            else
             {
-                if (!args.Contains("--todo"))
+                switch (args[0])
                 {
-                    WriteLine(@"When the 'new' command is used, the '--todo' flag must be provided, as well as its text.");
-                    return 1;
+                    case "new":
+                        ExecuteNew(args);
+                        break;
+                    case "list":
+                        ExecuteList();
+                        break;
+                }                
+            }
+            return 0;
+        }
+
+        public static int ExecuteNew(string[] args)
+        {
+            if (!args.Contains("--todo"))
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine(@"When the 'new' command is used, the '--todo' flag must be provided, as well as its text.");
+                ForegroundColor = ConsoleColor.Gray;
+                return 1;
+            }
+            else
+            {
+                int todoIndex = args.ToList().IndexOf("--todo");
+                if (todoIndex + 1 < args.Length && !string.IsNullOrEmpty(args[todoIndex + 1]))
+                {
+                    TodoText = args[todoIndex + 1];                    
+                    if (!Directory.Exists(QuestTodosPath))
+                    {
+                        Directory.CreateDirectory(QuestTodosPath);
+                        using (_ = File.Create(@$"{QuestTodosPath}\todos.txt")) {; }
+                    }
+                    else if (!File.Exists(@$"{QuestTodosPath}\todos.txt"))
+                    {
+                        using (_ = File.Create(@$"{QuestTodosPath}\todos.txt")) {; }
+                    }
+                    using (StreamWriter sw = File.AppendText($@"{QuestTodosPath}\todos.txt"))
+                        sw.WriteLine(TodoText);
+
+                    return 0;
                 }
                 else
                 {
-                    int todoIndex = args.ToList().IndexOf("--todo");
-                    if (todoIndex + 1 < args.Length && !string.IsNullOrEmpty(args[todoIndex + 1]))
-                    {
-                        TodoText = args[todoIndex + 1];                        
-                        string questTodosPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Quest";
-                        if (!Directory.Exists(questTodosPath)) 
-                        {
-                            Directory.CreateDirectory(questTodosPath);
-                            using (_ = File.Create(@$"{questTodosPath}\todos.txt")) {;}
-                        } 
-                        else if (!File.Exists(@$"{questTodosPath}\todos.txt"))
-                        {
-                            using (_ = File.Create(@$"{questTodosPath}\todos.txt")) {; }
-                        }
-                        using (StreamWriter sw = File.AppendText($@"{questTodosPath}\todos.txt"))                        
-                            sw.WriteLine(TodoText);
-                        
-                        return 0;
-                    }
-                    else
-                    {
-                        WriteLine("A text for the '--todo' flag must be provided.");
-                        return 1;
-                    }
-                    
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("A text for the '--todo' flag must be provided.");
+                    ForegroundColor = ConsoleColor.Gray;
+                    return 1;
                 }
+
+            }
+        }
+
+        public static int ExecuteList()
+        {
+            string[] lines = File.ReadAllLines(@$"{QuestTodosPath}\todos.txt");
+            foreach (string line in lines)
+            {
+                WriteLine(line);
             }
             return 0;
         }
