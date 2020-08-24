3a7582220401dd70c5acb0b3c4440a28b11808ec
Robson William <r.willup@hotmail.com>
2020-08-23 21:21:10 -0300

    Added two new unit tests

---
+++ src/QuestTests/Arguments/FeatHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using Quest.Arguments;
+using Quest.Models;
+using Xunit;
+
+namespace QuestTests.Arguments
+{
+    public class FeatHandlerTests
+    {
+        [Fact]
+        public void GetFeature_PassIfReturnsFeature()
+        {
+            // Arrange
+            string[] args = new string[] { "feat", "create", "Button", "--path", ".", "--desc", "A button" };
+
+            // Act
+            Feature feature = FeatHandler.GetFeature(args);
+
+            // Assert
+            Assert.NotNull(feature);
+        }
+
+        [Fact]
+        public void GetFeature_PassIfReturnsNull()
+        {
+            // Arrange
+            string[] args = new string[] { "feat", "create", "Button" };
+
+            // Act
+            Feature feature = FeatHandler.GetFeature(args);
+
+            // Assert
+            Assert.Null(feature);
+        }
+    }
+}
