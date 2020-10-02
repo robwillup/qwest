c754cb10c6c671a933076426cab39700d17d8728
Robson William <r.willup@hotmail.com>
2020-10-01 21:58:14 -0300

    Working on logic to create global config file

--- src/Quest/Program.cs
+++ src/Quest/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Quest
+﻿using System;
+using System.IO;
+
+namespace Quest
 {
     class Program
     {   
         static void Main(string[] args)
         {
+            // Check config file
+            string userProfilePath = Path.Combine(
+                                        Environment.GetFolderPath(
+                                            Environment.SpecialFolder.UserProfile), ".quest");
+            if (!Directory.Exists(userProfilePath))
+            {
+                string answer = "";
+                do {
+                    Console.WriteLine("Quest's global config not found.");
+                    Console.WriteLine("Would you like to create it now?");
+                    Console.WriteLine("[Y] Yes\n[N] No");
+                    answer = Console.ReadLine();
+                    answer = answer.ToLower();
+                } while (answer != "yes" && answer != "y" && answer != "n" && answer != "no");
+                if (answer == "no" || answer == "n")
+                {
+                    Console.WriteLine("Bye"); 
+                    return;
+                }
+                else
+                {
+                    Directory.CreateDirectory(userProfilePath);
+                    File.Create(Path.Combine(userProfilePath, "config.yml"));
+                }
+            }
+            // Work with arguments
             ToDoTests toDo = new ToDoTests();
             if (args[0] == "do")
                 toDo.Add(args[1]);
