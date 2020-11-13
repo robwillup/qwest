23b0d84920cbdf4183e3a61df21801818f1211cf
Robson William <r.willup@hotmail.com>
2020-11-12 21:54:27 -0300

    Added an else for undefined commands

--- src/Quest/ArgumentsHandler.cs
+++ src/Quest/ArgumentsHandler.cs
@@ -23,6 +23,8 @@ namespace Quest
                 toDo.Delete(args[1]);
             else if (args[0] == "version")
                 Console.WriteLine("1.0.0");
+            else
+                Console.WriteLine($"Command \"{args[0]}\" is undefined.");
 
             return true;
         }
