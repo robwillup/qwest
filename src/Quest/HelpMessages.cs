374b0e5b0c8a597044fcabe7901be72e3d440f2e
Robson William <r.willup@hotmail.com>
2020-12-10 23:12:36 -0300

    updated help message

--- src/Quest/HelpMessages.cs
+++ src/Quest/HelpMessages.cs
@@ -8,10 +8,12 @@ Quest!
 COMMANDS:
     do.............Creates a new task
     done...........Lists completed tasks
-    done <GUID>....Marks the task as complete
+    done...........Marks the task as complete
     todo...........Lists current active tasks
     undo...........Marks a complete task as active
     version........Displays current version
-    help...........Displays information about commands and flags";
+    help...........Displays information about commands and flags
+
+Use 'quest help <COMMAND>' for more information.";
     }
 }
