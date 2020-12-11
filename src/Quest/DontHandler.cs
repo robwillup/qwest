45d91fc0599c048ed0536f73f064c7f2c8a2f144
Robson William <r.willup@hotmail.com>
2020-12-10 23:38:02 -0300

    Added dont command

---
+++ src/Quest/DontHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Quest
+{
+    public static class DontHandler
+    {
+        public static bool Remove(string todo)
+        {
+            string todoPath = Path.Combine(Directory.GetCurrentDirectory(), "todo.md");
+            if (!File.Exists(todoPath))
+                return false;
+            List<string> todoContent = File.ReadAllLines(todoPath).ToList();
+            todoContent.Remove(todoContent.FirstOrDefault(t => t.Contains(todo)));
+            File.WriteAllLines(todoPath, todoContent);
+            return true;
+        }
+    }
+}
