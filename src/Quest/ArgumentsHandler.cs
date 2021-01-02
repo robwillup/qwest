2ef25767c3998a371cc19d4f77bd81068b2077df
Robson William <r.willup@hotmail.com>
2021-01-01 22:52:17 -0300

    refactored CommandHandler

--- src/Quest/ArgumentsHandler.cs
+++ src/Quest/ArgumentsHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Quest.Commands;
 
@@ -16,25 +15,9 @@ namespace Quest
             }
             await FileHandler.CreateTodoFile();
             string command = GetCommand(args);
-            if (command == "do")                
-                DoHandler.Add(DoHandler.Handle(args));
-            else if (command == "done")
-                DoneHandler.HandleDone(args);
-            else if (command == "todo")
-                ToDoHandler.List();
-            else if (command == "undo")
-                UndoHandler.Undo(args);
-            else if (command == "dont")
-                DontHandler.Remove(args);
-            else if (args[0] == "version")
-                Console.WriteLine("1.0.0");
-            else if (args[0] == "help")
-                HelpHandler.HandleHelp(args);
-            else
-                Console.WriteLine($"Command \"{args[0]}\" is undefined.");
-
-            return 0;
+            return CommandSelector.Select(args, command);
         }
+
         public static bool AnyArgument(string[] args) => args.Length > 0;
         public static string GetCommand(string[] args) => args[0];
         public static bool HasFlag(string[] args, string flag)
