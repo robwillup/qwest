4fe6f6466739aea86903987be9a93253e298a94d
Robson William <r.willup@hotmail.com>
2021-01-01 23:08:11 -0300

    Cleanup (#7)
    * return int from main
    
    * refactored CommandHandler
    
    * Added two new unit tests

---
+++ src/Quest/CommandSelector.cs
@@ -0,0 +1,29 @@
+﻿using Quest.Commands;
+using System;
+
+namespace Quest
+{
+    public static class CommandSelector
+    {
+        public static int Select(string[] args, string command)
+        {
+            if (command == "do")
+                DoHandler.Add(DoHandler.Handle(args));
+            else if (command == "done")
+                DoneHandler.HandleDone(args);
+            else if (command == "todo")
+                ToDoHandler.List();
+            else if (command == "undo")
+                UndoHandler.Undo(args);
+            else if (command == "dont")
+                DontHandler.Remove(args);
+            else if (args[0] == "version")
+                Console.WriteLine("1.0.0");
+            else if (args[0] == "help")
+                HelpHandler.HandleHelp(args);
+            else
+                Console.WriteLine($"Command \"{args[0]}\" is undefined.");
+            return 0;
+        }
+    }
+}
