7833d55fbf629a1c9348446b579f6a2c9119e335
Robson William <r.willup@hotmail.com>
2021-01-01 23:02:49 -0300

    Added two new unit tests

---
+++ src/QuestTests/ArgumentHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using Quest;
+using Xunit;
+
+namespace QuestTests
+{
+    public class ArgumentHandlerTests
+    {
+        [Fact]
+        public void TestAnyArgument_PassIfReturnsTrue()
+        {            
+            bool actual = ArgumentsHandler.AnyArgument(new string[1] { "one" });
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void TestAnyArgument_PassIfReturnsFalse()
+        {
+            bool actual = ArgumentsHandler.AnyArgument(new string[0]);
+            Assert.False(actual);
+        }
+    }
+}
