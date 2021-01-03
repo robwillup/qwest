5d5628754bab1e8aab4b7ade4f3e2d378b2c592e
Robson William <r.willup@hotmail.com>
2021-01-03 11:20:11 -0300

    new unit tests

--- src/QuestTests/FileHandlerTests.cs
+++ src/QuestTests/FileHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Quest;
 using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,6 +17,16 @@ namespace QuestTests
             await FileHandler.CreateTodoFileAsync("todo.md", file);
             Assert.True(File.Exists(file));
             File.Delete(file);
+        }
+
+        [Fact]
+        public void TestCreateContentStream()
+        {
+            string expectedString = "Hello Quest";            
+            MemoryStream contentStream = (MemoryStream)FileHandler.CreateContentStream(expectedString);
+            string actualString = Encoding.UTF8.GetString(contentStream.ToArray());
+            Assert.Equal(expectedString, actualString);
+
         }
     }
 }
