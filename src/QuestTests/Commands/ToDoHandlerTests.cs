9f440b27daf0e8a72158f0a76f8fe86ae7998917
Robson William <r.willup@hotmail.com>
2021-01-04 17:45:01 -0300

    Integration (#11)
    * new tests (#10)
    
    * commented unused code
    
    * new unit test for undo
    
    * changed todo dir

---
+++ src/QuestTests/Commands/ToDoHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Quest;
+using Quest.Commands;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QuestTests.Commands
+{
+    public class ToDoHandlerTests
+    {
+        [Fact]
+        public void TestList_PassIfNoFile()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            int actual = ToDoHandler.List(path);
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public async Task TestList_PassIfNoTasks()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "todo.md");
+            await FileHandler.CreateTodoFileAsync(filePath: path);
+            int actual = ToDoHandler.List(path);
+            File.Delete(path);
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public async Task TestList_PassIfThereAreTasks()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "todo.md");
+            await FileHandler.CreateTodoFileAsync(filePath: path);
+            DoHandler.Add("test", filePath: path);
+            int actual = ToDoHandler.List(path);
+            File.Delete(path);
+            Assert.Equal(0, actual);
+        }
+    }
+}
---
+++ src/QuestTests/Commands/UndoHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using Quest.Commands;
+using Xunit;
+
+namespace QuestTests.Commands
+{
+    public class UndoHandlerTests
+    {
+        [Fact]
+        public void TestUndo_PassIfReturns1()
+        {
+            string[] args = new string[] { "only one" };
+            int actual = UndoHandler.Undo(args);
+            Assert.Equal(1, actual);
+        }
+    }
+}
