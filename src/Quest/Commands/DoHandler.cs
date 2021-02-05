4f9e1b421a0a0b2311a6fcd78662b7d442d5391c
Robson William <r.willup@hotmail.com>
2021-02-04 21:33:19 -0300

    Removed console output from ConfigHandler

--- src/Quest/Commands/DoHandler.cs
+++ src/Quest/Commands/DoHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using static System.Console;
 using System.IO;
 using System.Collections.Generic;
 using Quest.Models;
---
+++ src/Quest/IO/ConfigDisplayer.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using Quest.Models;
+
+namespace Quest.IO
+{
+    public static class ConfigDisplayer
+    {
+        public static void DisplayConfig(Config config)
+        {
+            WriteLine($"Developer: {config.Dev.Username}");
+            WriteLine("Applications:");
+            foreach (App app in config.Applications)
+            {
+                WriteLine($"\tApp name: {app.Name}");
+                WriteLine($"\tLocal path: {app.LocalPath}");
+                WriteLine($"\tRemote: {app.Remote}");
+                if (app.Features != null)
+                {
+                    WriteLine("\tFeatures:");
+                    foreach (Feature feature in app.Features)
+                    {
+                        WriteLine($"\t\tFeature name: {feature.Name}");
+                    }
+                }
+                WriteLine();
+            }
+        }
+    }
+}
