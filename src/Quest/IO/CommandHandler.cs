6fc7380c7cde8348e4ebb7474526b1303a1dc2a7
Robson William <r.willup@hotmail.com>
2021-02-12 12:18:44 -0300

    Refactored CommandHandler

--- src/Quest/IO/CommandHandler.cs
+++ src/Quest/IO/CommandHandler.cs
@@ -8,32 +8,25 @@ namespace Quest
     {
         public static async Task<bool> RunAsync(string[] args, string command)
         {
-            try
-            {
-                if (command == "do")
-                    return await DoHandler.HandleAsync(args);
-                else if (command == "done")
-                    return await DoneHandler.HandleAsync(args);
-                else if (command == "todo")
-                    return ToDoHandler.List(ToDoHandler.Handle(args)) == 0;
-                else if (command == "undo")
-                    return UndoHandler.Handle(args);
-                else if (command == "dont")
-                    return await DontHandler.HandleAsync(args);
-                else if (command == "version")
-                    Console.WriteLine("1.0.0");
-                else if (command == "help")
-                    HelpHandler.HandleHelp(args);
-                else if (command == "config")
-                    ConfigHandler.Handle(args);
-                else
-                    Console.WriteLine($"Command \"{args[0]}\" is undefined.");
-                return true;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            if (command == "do")
+                return await DoHandler.HandleAsync(args);
+            else if (command == "done")
+                return await DoneHandler.HandleAsync(args);
+            else if (command == "todo")
+                return ToDoHandler.List(ToDoHandler.Handle(args)) == 0;
+            else if (command == "undo")
+                return UndoHandler.Handle(args);
+            else if (command == "dont")
+                return await DontHandler.HandleAsync(args);
+            else if (command == "version")
+                Console.WriteLine("1.0.0");
+            else if (command == "help")
+                HelpHandler.HandleHelp(args);
+            else if (command == "config")
+                ConfigHandler.Handle(args);
+            else
+                Console.WriteLine($"Command \"{args[0]}\" is undefined.");
+            return true;
         }
     }
 }
