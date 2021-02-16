48140f118c458f9a5ea70aedc54d90086c47a9c0
Robson William <r.willup@hotmail.com>
2021-02-16 20:40:07 -0300

    New unit tests for the Do command

--- src/QuestTests/CommandsTests/DoTests.cs
+++ src/QuestTests/CommandsTests/DoTests.cs
@@ -1,5 +1,4 @@
-﻿using Quest;
-using Quest.Commands;
+﻿using Quest.Commands;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -9,13 +8,36 @@ namespace QuestTests
 {
     public class DoTests
     {
-        [Fact]
-        public async Task TestHandleAsync()
+        private readonly string todoPath;
+        public DoTests()
         {
-            //Environment.SetEnvironmentVariable("QUEST_PATH", Path.Combine(Environment.CurrentDirectory));            
-            //string[] args = { "do", "test", "-a", "unit-test", "-f", "doCmd" };
-            //bool success = await Do.HandleAsync(args);
-            //Assert.True(success);
+            todoPath = Path.Combine(Environment.CurrentDirectory, ".quest", "doCmd", "todo.md");
+            Environment.SetEnvironmentVariable("QUEST_PATH", Environment.CurrentDirectory);
+        }
+
+        ~DoTests()
+        {
+            File.Delete(todoPath);
+        }
+
+        [Fact]
+        public async Task TestHandleAsync_PassIfReturnsTrue()
+        {            
+            string[] args = { "do", "TestHandleAsync_PassIfReturnsTrue", "-a", "unit-test", "-f", "doCmd" };
+            bool success = await Do.HandleAsync(args);            
+            Assert.True(success);
+        }
+
+        [Fact]
+        public async Task TestHandleAsync_PassIfTaskIsCreated()
+        {
+            string task = "TestHandleAsync_PassIfTaskIsCreated";
+            string[] args = { "do", task, "-a", "unit-test", "-f", "doCmd" };
+            _ = await Do.HandleAsync(args);
+            FileInfo file = new FileInfo(todoPath);
+            using StreamReader sr = new StreamReader(file.OpenRead());
+            string content = await sr.ReadToEndAsync();            
+            Assert.Contains(task, content);
         }
     }
 }
