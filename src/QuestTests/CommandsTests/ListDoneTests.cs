0bb85d5d11059cc153b52b3961c9dc3d71cfa8b6
Robson William <r.willup@hotmail.com>
2021-02-22 15:56:10 -0300

    Adding new unit test

---
+++ src/QuestTests/CommandsTests/ListDoneTests.cs
@@ -0,0 +1,16 @@
+﻿using Quest.Commands;
+using Xunit;
+
+namespace QuestTests.CommandsTests
+{
+    public class ListDoneTests
+    {
+        [Fact]
+        public void TestHandle_PassIfReturnsTrue()
+        {
+            string[] args = { "done" };
+            bool success = ListDone.Handle(args);
+            Assert.True(success);
+        }                
+    }
+}
